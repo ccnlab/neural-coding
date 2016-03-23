c16c19a1c725dd3cc1bab0291276c9ba7af5fed9
ccnlab <marcel.van.gerven@gmail.com>
2016-03-23 13:58:30 +0100

    bug fix
    as reported by Edward

--- external/GWP/Gabor_wavelet_pyramid_demo.m
+++ external/GWP/Gabor_wavelet_pyramid_demo.m
@@ -18,7 +18,8 @@ G = Gabor_wavelet_pyramid(b, FOV, gamma, lambda, sigma, theta);
 %%
 
 g    = cell(2, 1);
-g{1} = reshape(G{1}, size(G{1}, 1) * size(G{1}, 2), size(G{1}, 3));
+g{1} = reshape(G{1}, size(G{1}, 1) , size(G{1}, 2), size(G{1}, 3));
+%g{1} = reshape(G{1}, size(G{1}, 1) * size(G{1}, 2), size(G{1}, 3));
 g{2} = reshape(G{2}, size(g{1}));
 
 %%
@@ -34,8 +35,10 @@ x = reshape(X, size(X, 1) * size(X, 2), 1);
 
 % Simple cell responses
 
-s = g{1}' * x; % or s = G{2}' * x;
+s = g{1} * x; 
+%s = g{1}' * x; % or s = G{2}' * x;
 
 % Complex cell responses
 
-c = sqrt((g{1}' * x) .^ 2 + (g{2}' * x) .^ 2);
+%c = sqrt((g{1}' * x) .^ 2 + (g{2}' * x) .^ 2);
+c = sqrt((g{1} * x) .^ 2 + (g{2} * x) .^ 2);
